7be976d24febb9f61a18d63411ba8ca75fbf2c01
胡定锋 <hnhchdf@163.com>
2016-08-03 11:26:50 +0800

    Initial Commit

---
+++ JS_OCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JS_OCDemo
+//
+//  Created by hdf on 16/8/3.
+//  Copyright © 2016年 hdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JS_OCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JS_OCDemo
+//
+//  Created by hdf on 16/8/3.
+//  Copyright © 2016年 hdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
